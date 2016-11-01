78cf17d54c9963fac9af83b4b75b49a72fb8d352
Rafael Perez and Suzi Rubino <rafael+suzi@>
2016-11-01 14:06:01 -0700

    complete basic functionality of the movie ticket project.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ function UserChoice (age, movieSelection, matinee, cost){
   this.matinee = matinee;
   this.cost = [" ticket cost is $5.00", " ticket cost is $7.00", " ticket cost is $8.00", " ticket cost is $10.00"];
 }
+
 UserChoice.prototype.userOutput = function(price) {
+
+
   return this.movieSelection + this.matinee + this.cost[price];
   }
  // else if (this.movieSelection === 2 && this.matinee === 2) {
@@ -22,8 +25,19 @@ $(document).ready(function (){
 
     var newFinalChoice = new UserChoice(age, movieSelection, time);
     console.log(newFinalChoice);
+//set price here, using values from our array
+  var getCost = 0;
+  if (age === 0){
+    getCost = 0;
+  }else if(time === " anytime from noon to 5pm"){
+    getCost = 1;
+  }else if (movieSelection === "To watch Lord of the Flies"){
+    getCost = 2;
+  } else{
+    getCost = 3;
+  }
 
-    $("#output").text(newFinalChoice.userOutput(age));
+    $("#output").text(newFinalChoice.userOutput(getCost));
 
   });
 
